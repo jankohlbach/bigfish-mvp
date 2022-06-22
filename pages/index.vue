3ae3129f13e76cd18d55a79fce048c2be6bdd7a1
Jan Kohlbach <jan@dorfjungs.com>
2022-06-22 19:22:06 +0200

    ✨ now a floor with grid material

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,5 @@
 <template>
-  <div>
-    <canvas id="canvas"></canvas>
-  </div>
+  <canvas id="canvas"></canvas>
 </template>
 
 <script lang="ts">
@@ -16,7 +14,6 @@ export default Vue.extend({
     // const PANE = await import('tweakpane');
 
     const TEXTURE_PATH = "https://res.cloudinary.com/dg5nsedzw/image/upload/v1641657168/blog/vaporwave-threejs-textures/grid.png";
-    const DISPLACEMENT_PATH = "https://res.cloudinary.com/dg5nsedzw/image/upload/v1641657200/blog/vaporwave-threejs-textures/displacement.png";
 
     // const pane = new PANE.Pane();
 
@@ -28,34 +25,35 @@ export default Vue.extend({
 
     const scene = new THREE.Scene();
 
-    const fog = new THREE.Fog(0x000000, 1, 2.5);
-    scene.fog = fog;
+    // const fog = new THREE.Fog(0x000000, 1, 2.5);
+    // scene.fog = fog;
 
     const textureLoader = new THREE.TextureLoader();
     const gridTexture = textureLoader.load(TEXTURE_PATH);
-    const terrainTexture = textureLoader.load(DISPLACEMENT_PATH);
 
-    const geometry = new THREE.PlaneGeometry(1, 2, 24, 24);
+    const geometry = new THREE.CylinderGeometry(0.1, 0.1, 1, 4, 1, true);
     const material = new THREE.MeshStandardMaterial({
+      side: THREE.DoubleSide,
       map: gridTexture,
-      displacementMap: terrainTexture,
-      displacementScale: 0.4,
     });
     const plane = new THREE.Mesh(geometry, material);
     const plane2 = new THREE.Mesh(geometry, material);
 
-    plane.rotation.x = -Math.PI * 0.5;
-    plane.position.y = 0;
-    plane.position.z = 0.15;
+    plane.rotation.x = Math.PI / 2;
+    plane.rotation.y = Math.PI / 4;
+    plane.position.z = -0.5;
 
-    plane2.rotation.x = -Math.PI * 0.5;
-    plane2.position.y = 0;
-    plane2.position.z = -1.85;
+    plane2.rotation.x = Math.PI / 2;
+    plane2.rotation.y = Math.PI / 4;
+    plane2.position.z = -1.5;
 
     // const planeSettings = pane.addFolder({title: 'plane settings'});
-    // planeSettings.addInput(plane.rotation, 'x', {min: -Math.PI * 0.8, max: -Math.PI * 0.2});
-    // planeSettings.addInput(plane.position, 'y', {min: -1, max: 1});
-    // planeSettings.addInput(plane.position, 'z', {min: -1, max: 1});
+    // planeSettings.addInput(plane.rotation, 'x', {min: Math.PI / 4, max: Math.PI / 1});
+    // planeSettings.addInput(plane.rotation, 'y', {min: Math.PI / 4, max: Math.PI / 1});
+
+    // const plane2Settings = pane.addFolder({title: 'plane2 settings'});
+    // plane2Settings.addInput(plane2.rotation, 'x', {min: Math.PI / 4, max: Math.PI / 1});
+    // plane2Settings.addInput(plane2.rotation, 'y', {min: Math.PI / 4, max: Math.PI / 1});
 
     scene.add(plane);
     scene.add(plane2);
@@ -68,20 +66,18 @@ export default Vue.extend({
       height: window.innerHeight,
     };
 
-    const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.01, 20);
+    const camera = new THREE.PerspectiveCamera(50, sizes.width / sizes.height, 0.01, 1000);
 
-    camera.position.x = 0;
-    camera.position.y = 0.06;
-    camera.position.z = 1.1;
+    camera.position.y = -0.03;
+    camera.position.z = 0.5;
 
     // const cameraSettings = pane.addFolder({title: 'camera settings'});
-    // cameraSettings.addInput(camera.position, 'x', {min: -1, max: 1});
     // cameraSettings.addInput(camera.position, 'y', {min: -1, max: 1});
     // cameraSettings.addInput(camera.position, 'z', {min: 0, max: 2});
 
     const controls = new OrbitControls(camera, canvas);
     controls.enableDamping = true;
-    controls.enableZoom = false;
+    // controls.enableZoom = false;
 
     const renderer = new THREE.WebGLRenderer({canvas});
 
@@ -104,8 +100,8 @@ export default Vue.extend({
     const render = () => {
       const elapsedTime = clock.getElapsedTime();
 
-      plane.position.z = (elapsedTime * 0.15) % 2;
-      plane2.position.z = ((elapsedTime * 0.15) % 2) - 2;
+      plane.position.z = (elapsedTime * 0.1) % 1;
+      plane2.position.z = ((elapsedTime * 0.1) % 1) - 1;
 
       controls.update();
 
